9b8f27163d97b8524d2aa6b36bc512df91b1fff5
C_Minor <luca.campa.94@gmail.com>
2018-04-30 15:07:12 +0200

    Update package.json, initial version able for use

--- server/server.ts
+++ server/server.ts
@@ -7,7 +7,7 @@ import path = require('path');
 import { UserRouter } from './routes/userRouter';
 
 export const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const publicPath = path.join(__dirname, '../public');
 const userRouter = new UserRouter();
@@ -16,6 +16,6 @@ app.use(express.static(publicPath));
 app.use(bodyParser.json());
 app.use('/auth', userRouter.getRouter());
 
-app.listen(3000, (hostname: string) => {
+app.listen(port, (hostname: string) => {
     console.log(`CastleGate started at port ${port}`);
 });
