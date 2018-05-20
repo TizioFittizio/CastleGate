84a1aa134754311922d5c60d0a5f476edc954ae2
C_Minor <luca.campa.94@gmail.com>
2018-05-20 16:46:36 +0200

    Pre build commit test

--- server/db/mongoose.ts
+++ server/db/mongoose.ts
@@ -3,6 +3,7 @@ import mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI!)
 .catch(e => {
+    console.error('!');
     console.error('Unable to connect to mongo db: ' + e.message);
     process.exit(1);
 });
